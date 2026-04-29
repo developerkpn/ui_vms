741e42592c20ef87d9b80e4ab9660e9c29b4d4d4
DamdyJ <damdyjuns@gmail.com>
2026-04-29 14:12:33 +0700

    feat: add SearchSuggestionField skeleton

---
+++ src/components/common/SearchSuggestionField.jsx
@@ -0,0 +1,73 @@
+import { Autocomplete, TextField, Box, Typography, InputAdornment, IconButton } from "@mui/material";
+import { Search, Close } from "@mui/icons-material";
+import { useState, useCallback, useMemo } from "react";
+import { debounce } from "lodash";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+
+export default function SearchSuggestionField({ setQuery, placeholder }) {
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const axiosPrivate = useAxiosPrivate();
+
+  const fetchSuggestions = useMemo(() => 
+    debounce(async (term) => {
+      if (!term) {
+        setOptions([]);
+        return;
+      }
+      try {
+        const response = await axiosPrivate.get(`/material/search/all?pageSize=10&q=${encodeURIComponent(term)}`);
+        setOptions(response.data.data || []);
+      } catch (err) {
+        console.error("Suggestion fetch failed", err);
+      }
+    }, 800),
+    [axiosPrivate]
+  );
+
+  return (
+    <Autocomplete
+      open={open}
+      onOpen={() => setOpen(true)}
+      onClose={() => setOpen(false)}
+      inputValue={inputValue}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+        fetchSuggestions(newInputValue);
+      }}
+      onChange={(event, newValue) => {
+        if (newValue) {
+          // If code exists use it, otherwise name
+          setQuery(newValue.code || newValue.name);
+        }
+      }}
+      options={options}
+      getOptionLabel={(option) => option.combined_description || option.name || ""}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          placeholder={placeholder}
+          sx={{ 
+            width: "30rem",
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "8px",
+              backgroundColor: "#fff",
+            }
+          }}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setQuery(inputValue)} size="small">
+                  <Search />
+                </IconButton>
+                {params.InputProps.endAdornment}
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+}
